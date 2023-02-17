4ee30e9193409aaad1eb445e16aa1d21af3473a8
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-02-17 16:10:11 +0000

    added axios and search, now can search weather for user inputed city

--- src/WeatherToday.js
+++ src/WeatherToday.js
@@ -1,12 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+
 export default function WeatherToday() {
+  const [city, setCity] = useState("");
+  const [search, setSearch] = useState("");
+  const [displayName, setDisplayName] = useState("");
+  const [weather, setWeather] = useState("");
+  const [loaded, setLoaded] = useState(false);
+
+  function showWeather(response) {
+    setLoaded(true);
+    setDisplayName(response.data.name);
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      lowest: response.data.main.temp_min,
+      highest: response.data.main.temp_max,
+      feelsLike: response.data.main.feels_like,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    setSearch(city.trim());
+    if (search.length > 0) {
+      let apiKey = "7ed26a6948c661d05fafe7355b41b2ec";
+      let metricUnit = "units=metric";
+      let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&${metricUnit}`;
+      axios.get(apiUrl).then(showWeather);
+    }
+  }
+
+  function handleChange(event) {
+    setCity(event.target.value);
+  }
+
   let form = (
     <div className="SearchForm">
-      <form id="city-search" className="weather-search-form" autoComplete="off">
+      <form
+        onSubmit={handleSubmit}
+        id="city-search"
+        className="weather-search-form"
+        autoComplete="off"
+      >
         <div className="row">
           <div className="col-sm-10">
             <input
+              onChange={handleChange}
               id="inputed-city"
               type="search"
               className="search-bar form-control"
@@ -30,56 +73,68 @@ export default function WeatherToday() {
       </form>
     </div>
   );
-  return (
-    <div className="WeatherToday pagePaddingWeatherToday shadow">
-      {form}
-      <div className="CityName">
-        <h1 id="user-inputed-city" className="cityName">
-          Hong Kong
-        </h1>
-      </div>
-      <div className="DegreeNow">
-        <div id="degree-number" className="degreeNow">
-          10
+  if (loaded) {
+    return (
+      <div className="WeatherToday pagePaddingWeatherToday shadow">
+        {form}
+        <div className="CityName">
+          <h1 id="user-inputed-city" className="cityName">
+            {displayName}
+          </h1>
         </div>
-        <div className="degreeSign">
-          <span id="celsius" className="active">
-            °C{" "}
-          </span>
-          |<span id="fahrenheit">°F </span>
-        </div>
-      </div>
-      <div className="DegreeIcon">
-        <img
-          id="weatherNowIcon"
-          className="degreeNowIcon"
-          src={`https://openweathermap.org/img/wn/04n@2x.png`}
-          alt="{icon-id-{weatherIcons.iconID}"
-        />
-      </div>
-      <div className="WeatherDetails">
-        <div className="row weatherDetailsWholeRow">
-          <div className="col-sm-6 weatherDetailsColum">
-            <div className="weatherDetails">
-              <p id="weatherDetail-1"></p>
-              <p id="weatherDetail-2">Humidity : %</p>
-              <p id="weatherDetail-3">Wind :mps</p>
-            </div>
+        <div className="DegreeNow">
+          <div id="degree-number" className="degreeNow">
+            {Math.round(weather.temperature)}
           </div>
-          <div className="col-sm-6 degreeDetailsColum">
-            <div className="degreeDetails">
-              <p id="lowest-degree">Lowest : ° </p>
-              <p id="highest-degree">Highest: °</p>
-              <p id="feels-like-degree">Feels like: °</p>
-            </div>
+          <div className="degreeSign">
+            <span id="celsius" className="active">
+              °C{" "}
+            </span>
+            |<span id="fahrenheit">°F </span>
           </div>
         </div>
+        <div className="DegreeIcon">
+          <img
+            id="weatherNowIcon"
+            className="degreeNowIcon"
+            src={weather.icon}
+            alt={weather.icon}
+          />
+        </div>
+        <div className="WeatherDetails">
+          <div className="row weatherDetailsWholeRow">
+            <div className="col-sm-6 weatherDetailsColum">
+              <div className="weatherDetails">
+                <p id="weatherDetail-1">{weather.description}</p>
+                <p id="weatherDetail-2">Humidity :{weather.humidity} %</p>
+                <p id="weatherDetail-3">Wind :{weather.wind}mps</p>
+              </div>
+            </div>
+            <div className="col-sm-6 degreeDetailsColum">
+              <div className="degreeDetails">
+                <p id="lowest-degree">
+                  Lowest : {Math.round(weather.lowest)}°{" "}
+                </p>
+                <p id="highest-degree">
+                  Highest:{Math.round(weather.highest)}°
+                </p>
+                <p id="feels-like-degree">
+                  Feels like: {Math.round(weather.feelsLike)}°
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="LastUpdated">
+          <div id="last-updated-title">last updated</div>
+          <div id="date">Sat 25 Feb 2023</div>
+          <div id="last-updated-time">14:23</div>
+        </div>
       </div>
-      <div className="LastUpdated">
-        <div id="last-updated-title">last updated</div>
-        <div id="date">Sat 25 Feb 2023</div>
-        <div id="last-updated-time">14:23</div>
-      </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="WeatherToday pagePaddingWeatherToday shadow">{form}</div>
+    );
+  }
 }
