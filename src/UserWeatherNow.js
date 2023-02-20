197df3743d4e71478e09f788a548ff6c979e9c60
EvaSuper0115 <122745906+EvaSuper0115@users.noreply.github.com>
2023-02-20 12:20:16 +0000

    Now can convert high,low,feelslike degrees in UserWeatherNow.js for user current location

--- src/UserWeatherNow.js
+++ src/UserWeatherNow.js
@@ -2,15 +2,24 @@ import React, { useState } from "react";
 
 export default function UserWeatherNow(props) {
   const [temp, setTemp] = useState(props.userWeather.temperature);
+  const [lowTemp, setLowTemp] = useState(props.userWeather.lowest);
+  const [highTemp, setHighTemp] = useState(props.userWeather.highest);
+  const [feelsLike, setFeelsLike] = useState(props.userWeather.feelsLike);
 
   function convertToFahreheit(event) {
     event.preventDefault();
     setTemp((props.userWeather.temperature * 9) / 5 + 32);
+    setLowTemp((props.userWeather.lowest * 9) / 5 + 32);
+    setHighTemp((props.userWeather.highest * 9) / 5 + 32);
+    setFeelsLike((props.userWeather.feelsLike * 9) / 5 + 32);
   }
 
   function convertToCelsius(event) {
     event.preventDefault();
     setTemp(props.userWeather.temperature);
+    setLowTemp(props.userWeather.lowest);
+    setHighTemp(props.userWeather.highest);
+    setFeelsLike(props.userWeather.feelsLike);
   }
 
   return (
@@ -38,9 +47,9 @@ export default function UserWeatherNow(props) {
           </div>
           <div className="col-sm-6 degreeDetailsColum">
             <div className="degreeDetails">
-              <p>Lowest : {Math.round(props.userWeather.lowest)}° </p>
-              <p>Highest: {Math.round(props.userWeather.highest)}°</p>
-              <p>Feels like: {Math.round(props.userWeather.feelsLike)}°</p>
+              <p>Lowest : {Math.round(lowTemp)}° </p>
+              <p>Highest: {Math.round(highTemp)}°</p>
+              <p>Feels like: {Math.round(feelsLike)}°</p>
             </div>
           </div>
         </div>
